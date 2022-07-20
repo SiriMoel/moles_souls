ab29e0b69018c4792ceb5e630b61075b140d1a04
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-07-21 09:22:58 +1000

    solu blast "finished" (is untested)

--- files/scripts/projectiles/soul_blast.lua
+++ files/scripts/projectiles/soul_blast.lua
@@ -6,38 +6,22 @@ local x, y = EntityGetTransform( entity_id )
 
 local soul = EntityGetInRadiusWithTag( x, y, radius, "soul")[1]
 
---[[
-
-TODO
-
-orcs gives more explosion and radius + slithering path? + spark_green 
-
-mage gives more curse + projectile area teleport + spark_blue
-
-slime gives more cold + personal gravity field + spark_purple
-
-bat gives homing + spark_purple
-
-fly gives more projectile + chaotic path + spark_green
-
-spider gives matter eater + poison trail + venemous curse + less projectile + spark_pink (is that a thing?)
-
-gilded gives gold (the material) trail + ALL weakening curses + gold
-
-]]--
-
 local comp = EntityGetFirstComponent( entity_id, "ProjectileComponent" )
 local projdamage = ComponentGetValue2( comp, "damage" )
 local expdamage = ComponentObjectGetValue( comp, "config_explosion", "damage" )
 local exprad = ComponentObjectGetValue( comp, "config_explosion", "explosion_radius" )
 local cursedamage = ComponentObjectGetValue( comp, "damage_by_type", "curse" )
+local icedamage = ComponentObjectGetValue( comp, "damage_by_type", "ice" )
 
 
 if soul == nil then
+	GamePrint("You have no souls.")
     EntityKill(entity_id)
 end
 
-if EntityHasTag( soul, "soul_orcs" ) then
+
+--ORCS
+if EntityHasTag( soul, "soul_orcs" ) or EntityHasTag( soul, "soul_zombie" ) then
 
     EntityAddComponent( entity_id, "SineWaveComponent", {
         _enabled="1",
@@ -83,11 +67,67 @@ if EntityHasTag( soul, "soul_orcs" ) then
 
 end
 
+
+--GILDED
 if EntityHasTag( soul, "soul_gilded" ) then
 
     EntityAddComponent( entity_id, "ParticleEmitterComponent", {
         _enabled="1",
         emitted_material_name="gold",
+		x_pos_offset_min="0",
+		x_pos_offset_max="0",
+		y_pos_offset_min="0",
+		y_pos_offset_max="0",
+		x_vel_min="0",
+		x_vel_max="0",
+		y_vel_min="0",
+		y_vel_max="0",
+		count_min="60",
+		count_max="80",
+		lifetime_min="2.5",
+		lifetime_max="5.5",
+		is_trail="1",
+		trail_gap="1.0",
+		render_on_grid="1",
+		fade_based_on_lifetime="1",
+		airflow_force="0.1",
+		airflow_time="0.001",
+		airflow_scale="0.0833",
+		emission_interval_min_frames="1",
+		emission_interval_max_frames="1",
+		emit_cosmetic_particles="1",
+		create_real_particles="0",
+		is_emitting="1",
+    } )
+    
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/curse_wither_projectile.xml",
+	} )
+
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/curse_wither_melee.xml",
+	} )
+
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/curse_wither_explosion.xml",
+	} )
+
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/curse_wither_electricity.xml",
+	} )
+
+end
+
+--SPIDER
+if EntityHasTag( soul, "soul_spider" ) then
+	
+	EntityAddComponent( entity_id, "ParticleEmitterComponent", {
+        _enabled="1",
+        emitted_material_name="spark_purple",
 		x_pos_offset_min="0",
 		x_pos_offset_max="0",
 		y_pos_offset_min="0",
@@ -110,12 +150,26 @@ if EntityHasTag( soul, "soul_gilded" ) then
 		emission_interval_min_frames="1",
 		emission_interval_max_frames="1",
 		emit_cosmetic_particles="1",
-		create_real_particles="1",
+		create_real_particles="0",
 		is_emitting="1",
     } )
-    
+
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/curse_init.xml",
+	} )
+
+	EntityAddComponent( entity_id, "CellEaterComponent", { 
+		eat_probability="90",
+        radius="16",
+		ignored_material="rock_static_cursed",
+		ignored_material_tag="[matter_eater_ignore_list]",
+	} )
+
 end
 
+
+--MAGE
 if EntityHasTag( soul, "soul_mage" ) then
 
     EntityAddComponent( entity_id, "ParticleEmitterComponent", {
@@ -157,6 +211,141 @@ if EntityHasTag( soul, "soul_mage" ) then
 
     ComponentObjectSetValue( comp, "damage_by_type", "curse", cursedamage )
 
+end
+
+--SLIMES
+if EntityHasTag( soul, "soul_slimes" ) then
+	
+	EntityAddComponent( entity_id, "ParticleEmitterComponent", {
+        _enabled="1",
+        emitted_material_name="spark_green_bright",
+		x_pos_offset_min="0",
+		x_pos_offset_max="0",
+		y_pos_offset_min="0",
+		y_pos_offset_max="0",
+		x_vel_min="0",
+		x_vel_max="0",
+		y_vel_min="0",
+		y_vel_max="0",
+		count_min="20",
+		count_max="60",
+		lifetime_min="2.5",
+		lifetime_max="5.5",
+		is_trail="1",
+		trail_gap="1.0",
+		render_on_grid="1",
+		fade_based_on_lifetime="1",
+		airflow_force="0.1",
+		airflow_time="0.001",
+		airflow_scale="0.0833",
+		emission_interval_min_frames="1",
+		emission_interval_max_frames="1",
+		emit_cosmetic_particles="1",
+		create_real_particles="0",
+		is_emitting="1",
+    } )
+
+	EntityAddComponent( entity_id, "HitEffectComponent", { 
+		effect_hit="LOAD_UNIQUE_CHILD_ENTITY",
+		value_string="data/entities/misc/gravity_field_enemy.xml",
+	} )
+
+	icedamage = icedamage + 2
+	icedamage = icedamage * 2
+
+	ComponentObjectSetValue( comp, "damage_by_type", "ice", icedamage )
+
+end
+
+--FLY
+if EntityHasTag( soul, "soul_fly" ) then
+
+	EntityAddComponent( entity_id, "ParticleEmitterComponent", {
+        _enabled="1",
+        emitted_material_name="spark_red",
+		x_pos_offset_min="0",
+		x_pos_offset_max="0",
+		y_pos_offset_min="0",
+		y_pos_offset_max="0",
+		x_vel_min="0",
+		x_vel_max="0",
+		y_vel_min="0",
+		y_vel_max="0",
+		count_min="20",
+		count_max="60",
+		lifetime_min="2.5",
+		lifetime_max="5.5",
+		is_trail="1",
+		trail_gap="1.0",
+		render_on_grid="1",
+		fade_based_on_lifetime="1",
+		airflow_force="0.1",
+		airflow_time="0.001",
+		airflow_scale="0.0833",
+		emission_interval_min_frames="1",
+		emission_interval_max_frames="1",
+		emit_cosmetic_particles="1",
+		create_real_particles="0",
+		is_emitting="1",
+    } )	
+
+	EntityAddComponent( entity_id, "LuaComponent", {
+		script_source_file="data/scripts/projectiles/chaotic_arc.lua",
+		execute_every_n_frame="2",
+    } )
+
+	EntityAddComponent( entity_id, "HomingComponent", {
+		homing_targeting_coeff="130.0",
+		homing_velocity_multiplier="0.86",
+	} )
+
+	projdamage = projdamage + 1
+	projdamage = projdamage * 1.4
+
+	ComponentSetValue2( comp, "damage", projdamage )
+end
+
+
+--BAT
+if EntityHasTag( soul, "soul_bat" ) then
+
+	EntityAddComponent( entity_id, "ParticleEmitterComponent", {
+        _enabled="1",
+        emitted_material_name="spark_purple_bright",
+		x_pos_offset_min="0",
+		x_pos_offset_max="0",
+		y_pos_offset_min="0",
+		y_pos_offset_max="0",
+		x_vel_min="0",
+		x_vel_max="0",
+		y_vel_min="0",
+		y_vel_max="0",
+		count_min="20",
+		count_max="60",
+		lifetime_min="2.5",
+		lifetime_max="5.5",
+		is_trail="1",
+		trail_gap="1.0",
+		render_on_grid="1",
+		fade_based_on_lifetime="1",
+		airflow_force="0.1",
+		airflow_time="0.001",
+		airflow_scale="0.0833",
+		emission_interval_min_frames="1",
+		emission_interval_max_frames="1",
+		emit_cosmetic_particles="1",
+		create_real_particles="0",
+		is_emitting="1",
+    } )	
+
+	EntityAddComponent( entity_id, "HomingComponent", {
+		homing_targeting_coeff="130.0",
+		homing_velocity_multiplier="0.86",
+	} )
+
+	projdamage = projdamage * 1.2
+
+	ComponentSetValue2( comp, "damage", projdamage )
 end
 
 EntityKill(soul)
