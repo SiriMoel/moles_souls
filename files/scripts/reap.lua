2273699d13e5e673ccaf69adad58df83a995d028
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-07-22 10:30:43 +1000

    add souls.lua and make reap.lua use new methods

--- files/scripts/reap.lua
+++ files/scripts/reap.lua
@@ -1,34 +1,9 @@
 dofile("mods/moles_souls/files/utils.lua")
-
+local souls = dofile("mods/moles_souls/files/scripts/souls.lua")
 function death( damage_type_bit_field, damage_message, entity_thats_responsible, drop_items )
-
     local entity_id = GetUpdatedEntityID()
-    local x, y = EntityGetTransform( entity_id )
-
-    local player = EntityGetWithTag("player_unit")[1]
-    local px, py = EntityGetTransform(player)
-
-    local GenomeComp = EntityGetFirstComponent( entity_id, "GenomeDataComponent" )
-    local herd_id_number = ComponentGetValue2( GenomeComp, "herd_id")
+    local herd_id_number = ComponentGetValue2( EntityGetFirstComponent( entity_id, "GenomeDataComponent" ), "herd_id")
     local herd_id = HerdIdToString(herd_id_number)
-
     GamePrint(herd_id)
-
-    if ( "mods/moles_souls/files/entities/souls/soul_" .. herd_id .. ".xml" ) ~= nil then
-        
-        local is_gilded = math.random(1, 100)
-
-        local child_id
-
-        if is_gilded == 1 then 
-            child_id = EntityLoad( "mods/moles_souls/files/entities/souls/soul_gilded.xml", px, py )
-        else
-            child_id = EntityLoad( "mods/moles_souls/files/entities/souls/soul_" .. herd_id .. ".xml", px, py )
-        end
-    
-        EntityAddTag( child_id, "soul_entity" )
-        EntityAddChild( player, child_id ) 
-    end
-
-
+    souls:add(herd_id)
 end
---
+++ files/scripts/souls.lua
@@ -0,0 +1,42 @@
+dofile_once("mods/moles_souls/files/utils.lua")
+dofile_once("mods/moles_souls/lib/stringstore.lua")
+dofile_once("mods/moles_souls/lib/noitavariablestore.lua")
+local souls = {};
+local store = stringstore.open_store(stringstore.noita.variable_storage_components(EntityGetWithTag("player_unit")[1]))
+                  .souls;
+GamePrint(type(store))
+
+function spawn(herd)
+    GamePrint(herd)
+    local player = EntityGetWithTag("player_unit")[1]
+    local px, py = EntityGetTransform(player)
+    if herd == "synthetic" then
+        EntityAddChild(player, EntityLoad("mods/moles_souls/files/entities/souls/soul_synthetic.xml", px, py))
+        return
+    end
+    if ("mods/moles_souls/files/entities/souls/soul_" .. herd .. ".xml") ~= nil then
+        local is_gilded = math.random(1, 100)
+        if is_gilded == 1 then
+            herd = "guilded"
+        end
+        local child_id = EntityLoad("mods/moles_souls/files/entities/souls/soul_" .. herd .. ".xml", px, py)
+        EntityAddChild(player, child_id)
+    end
+end
+
+function kill(herd)
+    local tag = "soul_" .. herd
+    if herd == "any" then tag = "soul" end
+    local souls = EntityGetWithTag(tag)
+    EntityKill(souls[math.random(1, #souls)])
+end
+
+function souls:add(herd)
+    spawn(herd);
+end
+
+function souls:remove(herd)
+    kill(herd);
+end
+
+return souls
