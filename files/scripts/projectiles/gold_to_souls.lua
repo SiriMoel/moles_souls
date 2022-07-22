2a2aa08cdbcd8be2aaefe2005e44466ee44e49f8
Oliver Fox-Smith <ofoxsmith@outlook.com>
2022-07-22 11:03:13 +1000

    update gold to souls to use new methods

--- files/scripts/projectiles/gold_to_souls.lua
+++ files/scripts/projectiles/gold_to_souls.lua
@@ -1,24 +1,15 @@
 dofile("mods/moles_souls/files/utils.lua")
-
-local player = EntityGetWithTag( "player_unit" )[1]
-local px, py = EntityGetTransform( player )
+local souls = dofile("mods/moles_souls/files/scripts/souls.lua")
 
 local walletcomp = EntityGetFirstComponent( player, "WalletComponent" )
 
 if walletcomp ~= nil then
     local money = ComponentGetValue2( walletcomp, "money" )
-    local moneyspent = ComponentGetValue2( walletcomp, "money_spent" )
-
-    local child_id = EntityLoad( "mods/moles_souls/files/entities/souls/soul_synthetic.xml", px, py )
-
     if money * 0.05 >= 1000 then
-
         local cost = 1000 + ( money * 0.5 )
-
         ComponentSetValue2( walletcomp, "money", money - cost)
-        ComponentSetValue2( walletcomp, "money_spent", moneyspent + cost)
-        EntityAddChild( player, EntityLoad( "mods/moles_souls/files/entities/souls/soul_synthetic.xml", px, py ) ) 
-
+        ComponentSetValue2( walletcomp, "money_spent", ComponentGetValue2( walletcomp, "money_spent" ) + cost)
+        souls:add("synthetic")
     else
         GamePrint("You are too poor!")
     end
