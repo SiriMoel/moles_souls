69fa0d7213ebd2ea48d1e070987ea5f71266c38f
Mir Sole <65946597+ZacIsBurnt@users.noreply.github.com>
2022-07-30 15:22:04 +1000

    testing purposes

--- files/scripts/projectiles/lantern.lua
+++ files/scripts/projectiles/lantern.lua
@@ -6,6 +6,8 @@ local entity_id = GetUpdatedEntityID()
 local root_id = EntityGetRootEntity( entity_id )
 local x, y = EntityGetTransform( entity_id )
 
+EntityLoad("mods/moles_souls/files/particles/ritual_catalyst.xml", x, y)
+
 local soul_count = souls:count()
 
 local count = soul_count * 0.5 + 1
